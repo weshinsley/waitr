5b1fcb59fb5d88227bf77b5e27a17dec28ffb68d
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-06-19 11:16:31 +0100

    Improve docs

--- R/waitr.R
+++ R/waitr.R
@@ -1,47 +1,44 @@
-#' Functions to support sleeping for a specific number of milliseconds,
-#' or wait until the relative time has reached a certain deadline in
-#' milliseconds. This is lower overhead than Sys.sleep, and the use of the
-#' relative time allows both taking up any slack, or catching up on lost time.
-#' Likely usage is for simple animation, or any code you
-#' would like to run at similar speeds on different systems, by simple tim3e
-#' synchronisation.
-#'
-#' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
-#' @export
-
-#' Sleep for a given number of milliseconds.
-#' @param delay_ms Milliseconds to wait.
-#' @return The time in milliseconds, relative to the last reset.
-#'
-#' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
-#' @export
+##' @description This package provides an internal clock measured in milliseconds since
+##' the last reset, with functions to get the current timestamp, reset it to
+##' zero, sleep for a number of milliseconds, or sleep until the relative time
+##' is beyond a certain number of milliseconds. All functions return the relative
+##' time in millseconds at the end of the function call.
+##'
+##' This aim is to provide a lower overhead alternative to Sys.sleep, for
+##' the purpose of simple animation at a reasonably consistent frame-rate,
+##' regardless of platform and system.
+##'
+##' @title Sleep for, or until a given time
+##' @rdname waitr
+##' @param delay_ms Milliseconds to wait.
+##' @return The time in milliseconds, relative to the last reset.
+##' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
+##' @export
 waitr_delay <- function(delay_ms) {
   .Call(C_waitr_delay, as.integer(delay_ms))
 }
 
-#' Sleep until the relative time is on, or after a specified time.
-#' @param time_ms The time in milliseconds since the last reset. If that time
-#' is in the future, waitr will sleep until that time.
-#' @return The new time in milliseconds, relative to the last reset.
-#'
-#' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
-#' @export
+##' @title Sleep until the relative time is beyond a specified time
+##' @param time_ms Clock time to wait for, in milliseconds.
+##' @rdname waitr
+##' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
+##' @export
 waitr_until <- function(time_ms) {
   .Call(C_waitr_until, as.integer(time_ms))
 }
 
-#' Get the current time in milliseconds, relative to the last reset.
-#' @return The current relative time in milliseconds.
-#' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
-#' @export
+##' @title Get current time in milliseconds, relative to the last reset
+##' @rdname waitr
+##' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
+##' @export
 waitr_timestamp <- function() {
   .Call(C_waitr_timestamp)
 }
 
-#' Reset the current time in milliseconds to zero.
-#' @return The new time in milliseconds - zero.
-#' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
-#' @export
+##' @title Reset the current time to zero
+##' @rdname waitr
+##' @useDynLib waitr, .registration = TRUE, .fixes = "C_"
+##' @export
 waitr_reset <- function() {
   .Call(C_waitr_reset)
 }
